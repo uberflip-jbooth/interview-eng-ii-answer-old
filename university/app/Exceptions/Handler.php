8c9cbe4b20858be0329eaf2c44b378df5e67cf76
uberflip-jbooth <james.booth@uberflip.com>
2021-12-03 13:29:51 +0000

    Added error handling

--- university/app/Exceptions/Handler.php
+++ university/app/Exceptions/Handler.php
@@ -4,9 +4,16 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Traits\APIResponder;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
+    use APIResponder;
+
     /**
      * A list of the exception types that are not reported.
      *
@@ -32,10 +39,38 @@ class Handler extends ExceptionHandler
      *
      * @return void
      */
-    public function register()
+    public function register(): void
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+        $this->renderable(function (ModelNotFoundException $e, $request) {
+            return $this->error([
+                'title' => 'Error: Not Found',
+                'detail' => 'Record for '.$e->getModel().' not found.',
+                'type' => $e->getModel(),
+            ], 404);
+        });
+        $this->renderable(function (NotFoundHttpException $e, $request) {
+            return $this->error([
+                'title' => 'Error: Not Found',
+                'detail' => $e->getMessage(),
+                'type' => 'notfound\error',
+            ], 404);
+        });
+        $this->renderable(function (ValidationException $e, $request) {
+            return $this->error([
+                'title' => 'Error: Invalid data',
+                'detail' => $e->errors(),
+                'type' => 'validation\error',
+            ], $e->status);
+        });
+        $this->renderable(function (AuthenticationException $e, $request) {
+            return $this->error([
+                'title' => 'Error: Unauthenticated',
+                'detail' => implode(',', $e->guards()),
+                'type' => 'auth\error',
+            ], 401);
         });
     }
 }
