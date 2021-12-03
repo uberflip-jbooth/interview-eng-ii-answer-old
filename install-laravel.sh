a0f20de4cb2ba97f7d0fa8c994b0cd04409b7e42
uberflip-jbooth <james.booth@uberflip.com>
2021-12-03 14:24:17 +0000

    Changes for Sail

--- install-laravel.sh
+++ install-laravel.sh
@@ -24,9 +24,13 @@ sudo chown -R $USER: .
 sed -i 's/APP_NAME=.*/APP_NAME=University/' .env.example
 sed -i 's/APP_URL=.*/APP_URL="${GITPOD_WORKSPACE_URL}"/' .env.example
 sed -i '/^APP_URL/a APP_PORT=9080' .env.example
-sed -i 's/DB_HOST=.*/DB_HOST=0.0.0.0/' .env.example
+sed -i 's/DB_HOST=.*/DB_HOST=db/' .env.example
 sed -i 's/DB_USERNAME=.*/DB_USERNAME=uberflip/' .env.example
 sed -i 's/DB_PASSWORD=.*/DB_PASSWORD=pass123/' .env.example
+
+# Modify the Sail docker-compose to work with our external network
+sed -i 's/- sail/- sail\n            - local/g' docker-compose.yml
+sed -i '/^networks:/a\    local:\n        external: true' docker-compose.yml
 
 cd ..
 
